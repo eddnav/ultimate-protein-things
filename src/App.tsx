f5797a6e678bd7c568c15da5047f76eb8a5097de
Eduardo Naveda <eduardo.naveda@ah.nl>
2024-08-01 23:46:34 +0200

    Add pretier

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,72 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import { Review } from './models/Review';
-import { Column, ColumnFiltersState, createColumnHelper, ExpandedState, flexRender, getCoreRowModel, getExpandedRowModel, getFacetedMinMaxValues, getFacetedRowModel, getFacetedUniqueValues, getFilteredRowModel, RowData, useReactTable } from '@tanstack/react-table';
-import { ProductType } from './models/ProductType';
-import 'bulma/css/bulma.min.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Review } from "./models/Review";
+import {
+  ColumnFiltersState,
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  getFacetedMinMaxValues,
+  getFacetedRowModel,
+  getFacetedUniqueValues,
+  getFilteredRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import "bulma/css/bulma.min.css";
 import "@fortawesome/fontawesome-free/css/all.css";
-import { Tier } from './models/Tier';
-import Filter from './Filter';
-import { ReviewCard } from './ReviewCard';
-
-// TODO: Fix formatting eslint + prettier
-// TODO: Fix favico and logos
-
+import Filter from "./Filter";
+import { ReviewCard } from "./ReviewCard";
 
 function App() {
-
   const [data, setData] = useState<Review[]>([]);
-  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>(
-    []
-  )
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
 
-  const helper = createColumnHelper<Review>()
+  const helper = createColumnHelper<Review>();
 
   const columns = [
-    helper.accessor('product.imageUrl', {
+    helper.accessor("product.imageUrl", {
       header: () => <></>,
-      enableColumnFilter: false
+      enableColumnFilter: false,
     }),
-    helper.accessor('product.brand', {
+    helper.accessor("product.brand", {
       header: () => <span>Brand</span>,
       meta: {
-        filterVariant: 'select',
-      }
+        filterVariant: "select",
+      },
     }),
-    helper.accessor('product.name', {
+    helper.accessor("product.name", {
       header: () => <span>Product</span>,
       meta: {
-        filterVariant: 'text',
-      }
+        filterVariant: "text",
+      },
     }),
-    helper.accessor('product.type', {
+    helper.accessor("product.type", {
       header: () => <span>Type</span>,
-      filterFn: 'equalsString',
+      filterFn: "equalsString",
       meta: {
-        filterVariant: 'select',
-      }
+        filterVariant: "select",
+      },
     }),
-    helper.accessor('product.caloriesInKcal', {
+    helper.accessor("product.caloriesInKcal", {
       header: () => <span>Calories (kcal)</span>,
       meta: {
-        filterVariant: 'range',
-      }
+        filterVariant: "range",
+      },
     }),
-    helper.accessor('product.proteinInGrams', {
+    helper.accessor("product.proteinInGrams", {
       header: () => <span>Protein (g)</span>,
       meta: {
-        filterVariant: 'range',
-      }
+        filterVariant: "range",
+      },
     }),
-    helper.accessor('tier', {
-      header: () => <span className='text-ellipsis'>Tier</span>,
-      filterFn: 'equalsString',
+    helper.accessor("tier", {
+      header: () => <span className="text-ellipsis">Tier</span>,
+      filterFn: "equalsString",
       meta: {
-        filterVariant: "select"
-      }
-    })
-  ]
+        filterVariant: "select",
+      },
+    }),
+  ];
 
   const table = useReactTable({
     data,
@@ -81,12 +82,14 @@ function App() {
     getFacetedMinMaxValues: getFacetedMinMaxValues(),
     debugTable: true,
     debugHeaders: true,
-    debugColumns: false
-  })
+    debugColumns: false,
+  });
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('https://raw.githubusercontent.com/eddnav/ultimate-protein-things/main/data.json');
+      const response = await fetch(
+        "https://raw.githubusercontent.com/eddnav/ultimate-protein-things/main/data.json"
+      );
       const result: [Review] = await response.json();
       setData(result);
     };
@@ -95,18 +98,21 @@ function App() {
 
   return (
     <section className="section">
-      <div className='container mb-6'>
-        <div className='content is-medium'>
+      <div className="container mb-6">
+        <div className="content is-medium">
           <h1>Protein Things, Reviewed</h1>
-          <p>Judgement passed upon unsuspecting protein products by <a href='https://github.com/eddnav'>this guy</a>.</p>
+          <p>
+            Judgement passed upon unsuspecting protein products by{" "}
+            <a href="https://github.com/eddnav">this guy</a>.
+          </p>
         </div>
-        <div className='columns is-centered'>
-          {table.getHeaderGroups().map(headerGroup => (
-            headerGroup.headers.map(header => (
+        <div className="columns is-centered">
+          {table.getHeaderGroups().map((headerGroup) =>
+            headerGroup.headers.map((header) =>
               header.column.getCanFilter() ? (
-                <div className='column is-desktop' key={header.id}>
-                  <div className='mb-2'>
-                    <div className='mb-1'>
+                <div className="column is-desktop" key={header.id}>
+                  <div className="mb-2">
+                    <div className="mb-1">
                       {flexRender(
                         header.column.columnDef.header,
                         header.getContext()
@@ -116,14 +122,16 @@ function App() {
                       <Filter column={header.column} />
                     </div>
                   </div>
-                </div>) : null
-            ))))}
+                </div>
+              ) : null
+            )
+          )}
         </div>
       </div>
-      <div className='container is-fluid'>
-        <div className='masonry-columns'>
-          {table.getRowModel().rows.map(row => (
-            <div className='masonry-item' key={row.id}>
+      <div className="container is-fluid">
+        <div className="masonry-columns">
+          {table.getRowModel().rows.map((row) => (
+            <div className="masonry-item" key={row.id}>
               <ReviewCard row={row} />
             </div>
           ))}
--- src/Filter.tsx
+++ src/Filter.tsx
@@ -1,116 +1,124 @@
-import { Column, RowData } from "@tanstack/react-table"
-import React, { useEffect, useState } from "react"
-import { ProductType } from "./models/ProductType"
+import { Column, RowData } from "@tanstack/react-table";
+import React, { useEffect, useState } from "react";
+import { ProductType } from "./models/ProductType";
 
-declare module '@tanstack/react-table' {
-    interface ColumnMeta<TData extends RowData, TValue> {
-        filterVariant?: 'text' | 'range' | 'select'
-    }
+declare module "@tanstack/react-table" {
+  // eslint-disable-next-line
+  interface ColumnMeta<TData extends RowData, TValue> {
+    filterVariant?: "text" | "range" | "select";
+  }
 }
 
 function toKeyAndUserFacingStringTuple(key: any): [any, any] {
-    if (Object.keys(ProductType).includes(key)) {
-        return [key, ProductType.toString(key)]
-    } else {
-        return [key, key]
-    }
+  if (Object.keys(ProductType).includes(key)) {
+    return [key, ProductType.toString(key)];
+  } else {
+    return [key, key];
+  }
 }
 
 function Filter({ column }: { column: Column<any, unknown> }) {
+  const { filterVariant } = column.columnDef.meta ?? {};
 
-    const { filterVariant } = column.columnDef.meta ?? {}
+  const columnFilterValue = column.getFilterValue();
 
-    const columnFilterValue = column.getFilterValue()
+  const uniqueValues = React.useMemo(
+    () =>
+      filterVariant === "range"
+        ? []
+        : Array.from(column.getFacetedUniqueValues().keys())
+            .map(toKeyAndUserFacingStringTuple)
+            .slice(0, 5000),
+    // eslint-disable-next-line
+    [column.getFacetedUniqueValues(), filterVariant]
+  );
 
-    const uniqueValues = React.useMemo(
-        () =>
-            filterVariant === 'range'
-                ? []
-                : Array.from(column.getFacetedUniqueValues().keys())
-                    .map(toKeyAndUserFacingStringTuple)
-                    .slice(0, 5000),
-        [column.getFacetedUniqueValues(), filterVariant]
-    )
-
-    return filterVariant === 'range' ? (
-        <div className='columns is-1'>
-            <div className='column'>
-                <DebouncedInput
-                    value={(columnFilterValue as [number, number])?.[0] ?? ''}
-                    onChange={value =>
-                        column.setFilterValue((old: [number, number]) => [value, old?.[1]])
-                    }
-                    placeholder={`Min ${column.getFacetedMinMaxValues()?.[0] !== undefined
-                        ? `(${column.getFacetedMinMaxValues()?.[0]})`
-                        : ''
-                        }`}
-                />
-            </div>
-            <div className='column'>
-                <DebouncedInput
-                    value={(columnFilterValue as [number, number])?.[1] ?? ''}
-                    onChange={value =>
-                        column.setFilterValue((old: [number, number]) => [old?.[0], value])
-                    }
-                    placeholder={`Max ${column.getFacetedMinMaxValues()?.[1]
-                        ? `(${column.getFacetedMinMaxValues()?.[1]})`
-                        : ''
-                        }`}
-                />
-            </div>
-        </div>
-    ) : filterVariant === 'select' ? (
-        <div className="select is-fullwidth">
-            <select
-                onChange={e => column.setFilterValue(e.target.value)}
-                value={columnFilterValue?.toString()}
-            >
-                <option value="">All</option>
-                {uniqueValues.map(value => (
-                    <option value={value[0]} key={value[0]}>
-                        {value[1]}
-                    </option>
-                ))}
-            </select>
-        </div>
-    ) : (
+  return filterVariant === "range" ? (
+    <div className="columns is-1">
+      <div className="column">
         <DebouncedInput
-            type="text"
-            value={(columnFilterValue ?? '') as string}
-            onChange={value => column.setFilterValue(value)}
-            placeholder={`Search... (${column.getFacetedUniqueValues().size})`}
+          value={(columnFilterValue as [number, number])?.[0] ?? ""}
+          onChange={(value) =>
+            column.setFilterValue((old: [number, number]) => [value, old?.[1]])
+          }
+          placeholder={`Min ${
+            column.getFacetedMinMaxValues()?.[0] !== undefined
+              ? `(${column.getFacetedMinMaxValues()?.[0]})`
+              : ""
+          }`}
         />
-    )
+      </div>
+      <div className="column">
+        <DebouncedInput
+          value={(columnFilterValue as [number, number])?.[1] ?? ""}
+          onChange={(value) =>
+            column.setFilterValue((old: [number, number]) => [old?.[0], value])
+          }
+          placeholder={`Max ${
+            column.getFacetedMinMaxValues()?.[1]
+              ? `(${column.getFacetedMinMaxValues()?.[1]})`
+              : ""
+          }`}
+        />
+      </div>
+    </div>
+  ) : filterVariant === "select" ? (
+    <div className="select is-fullwidth">
+      <select
+        onChange={(e) => column.setFilterValue(e.target.value)}
+        value={columnFilterValue?.toString()}
+      >
+        <option value="">All</option>
+        {uniqueValues.map((value) => (
+          <option value={value[0]} key={value[0]}>
+            {value[1]}
+          </option>
+        ))}
+      </select>
+    </div>
+  ) : (
+    <DebouncedInput
+      type="text"
+      value={(columnFilterValue ?? "") as string}
+      onChange={(value) => column.setFilterValue(value)}
+      placeholder={`Search... (${column.getFacetedUniqueValues().size})`}
+    />
+  );
 }
 
 function DebouncedInput({
-    value: initialValue,
-    onChange,
-    debounce = 500,
-    ...props
+  value: initialValue,
+  onChange,
+  debounce = 500,
+  ...props
 }: {
-    value: string | number
-    onChange: (value: string | number) => void
-    debounce?: number
-} & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'>) {
+  value: string | number;
+  onChange: (value: string | number) => void;
+  debounce?: number;
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange">) {
+  const [value, setValue] = useState(initialValue);
 
-    const [value, setValue] = useState(initialValue)
+  useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
 
-    useEffect(() => {
-        setValue(initialValue)
-    }, [initialValue])
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value);
+    }, debounce);
 
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            onChange(value)
-        }, debounce)
+    return () => clearTimeout(timeout);
+    // eslint-disable-next-line
+  }, [value]);
 
-        return () => clearTimeout(timeout)
-    }, [value])
-
-    return (
-        <input className="input" {...props} value={value} onChange={e => setValue(e.target.value)} />
-    )
+  return (
+    <input
+      className="input"
+      {...props}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
 }
 
-export default Filter
+export default Filter;
